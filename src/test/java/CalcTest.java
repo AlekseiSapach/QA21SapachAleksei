622813de3181cbb961e14fe03aabe513d83b59b2
Aleksei1364 <sapach1364@gmail.com>
2023-06-22 20:56:51 +0300

    commit lessons 2

--- src/test/java/CalcTest.java
+++ src/test/java/CalcTest.java
@@ -10,7 +10,14 @@ public class CalcTest extends BaseTest {
         int actualValue = calculator.sum(2,3);
 
         Assert.assertEquals(actualValue,expectedValue,"Значения не одинаковые");
+    }
+    @Test(enabled = false)
+    public void sumTest_2()
+    {
+        int expectedValue = 5;
+        int actualValue = calculator.sum(2,3);
 
+        Assert.assertEquals(actualValue,expectedValue,"Значения не одинаковые");
     }
 
 
