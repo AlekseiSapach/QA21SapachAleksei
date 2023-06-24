915caffae75900c047b60113939903fd311fd272
Aleksei1364 <sapach1364@gmail.com>
2023-06-24 15:58:39 +0300

    commit lessons 2

---
+++ src/test/java/HomeWorkTest.java
@@ -0,0 +1,35 @@
+import data.StaticProvider;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class HomeWorkTest extends BaseTest
+{
+    @Test (testName = "Целочисленное деление",groups = "integer Value",
+            dataProvider = "Data for Div Int", dataProviderClass = StaticProvider.class)
+    public void testDivInt(int a, int b, int expectedValue)
+    {
+        Assert.assertEquals(calculator.divInt(a,b),expectedValue,"Значения не одинаковые");
+    }
+    @Test (testName = "Деление на ноль целого числа",groups = {"integer Value","division byZero"}, dataProvider = "Data for Div Int Zero",
+            dataProviderClass = StaticProvider.class, expectedExceptions = ArithmeticException.class)
+    public void testDivIntZero(int a, int b, Object expectedValue)
+    {
+        Assert.assertEquals(calculator.divInt(a,b),expectedValue,"Значения не одинаковые");
+    }
+
+    @Test (testName = "Деление с остатком",groups = "decimal Value",
+            dataProvider = "Data for Div Double", dataProviderClass = StaticProvider.class)
+    public void testDivDouble(double a, double b, double expectedValue)
+    {
+        Assert.assertEquals(calculator.divDouble(a,b),expectedValue,"Значения не одинаковые");
+    }
+    @Test (testName = "Деление с остатком на ноль",groups = {"decimal Value","division byZero"},
+            dataProvider = "Data for Div Double Zero", dataProviderClass = StaticProvider.class)
+    public void testDivDoubleZero(double a, double b, double expectedValue)
+    {
+        Assert.assertEquals(calculator.divDouble(a,b),expectedValue,"Значения не одинаковые");
+    }
+
+}
--- src/test/java/data/StaticProvider.java
+++ src/test/java/data/StaticProvider.java
@@ -12,4 +12,39 @@ public class StaticProvider {
                 {2,3,5}
         };
     }
+    @DataProvider(name = "Data for Div Int")
+    public static Object[][] dataForDivInt(){
+        return new Object[][]{
+                {10,2,5},
+                {15,6,2},
+                {-8,2,-4},
+                {-10,-2,5},
+                {0,8,0},
+        };
+    }
+    @DataProvider(name = "Data for Div Int Zero")
+    public static Object[][] dataForDivIntZero() {
+        return new Object[][]{
+                {10, 0, ArithmeticException.class},
+                {0,0,ArithmeticException.class}
+        };
+    }
+    @DataProvider(name = "Data for Div Double")
+    public static Object[][] dataForDivDouble(){
+        return new Object[][]{
+                {10.5,3.5,3.0},
+                {8.4,16.8,0.5},
+                {-22.5,4.5,-5.0},
+                {-8.4,-16.8,0.5},
+                {0,2.5,0}
+        };
+    }
+    @DataProvider(name = "Data for Div Double Zero")
+    public static Object[][] dataForDivDoubleZero(){
+        return new Object[][]{
+                {0d,0,Double.NaN},
+                {1.2d,0,Double.POSITIVE_INFINITY},
+                {-1.2d,0,Double.NEGATIVE_INFINITY}
+        };
+    }
 }
