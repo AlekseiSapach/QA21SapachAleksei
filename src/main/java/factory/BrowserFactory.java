6d47435168812f172f4ea990c7a94acf670270cc
Aleksei1364 <sapach1364@gmail.com>
2023-07-21 20:37:48 +0300

    advancedActions

--- src/main/java/factory/BrowserFactory.java
+++ src/main/java/factory/BrowserFactory.java
@@ -9,50 +9,64 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.firefox.FirefoxOptions;
 import utils.configuration.ReadProperties;
 
+import java.time.Duration;
+
 public class BrowserFactory {
     private WebDriver driver = null;
     private DriverManagerType driverManagerType = null;
-    public BrowserFactory(){
-        switch (ReadProperties.browserName().toLowerCase()){
-            case "chrome":
+
+    public BrowserFactory() {
+        switch (ReadProperties.browserName().toLowerCase()) {
+            case "chrome" :
                 driverManagerType = DriverManagerType.CHROME;
-                WebDriverManager.getInstance(driverManagerType).setup();
+                //WebDriverManager.getInstance(driverManagerType).setup();
+                WebDriverManager.chromedriver().driverVersion("114.0.5735.90").setup();
+
                 driver = new ChromeDriver(getChromeOptions());
                 break;
             case "firefox":
                 driverManagerType = DriverManagerType.FIREFOX;
                 WebDriverManager.getInstance(driverManagerType).setup();
+
                 driver = new FirefoxDriver(getFirefoxOptions());
                 break;
             default:
-                System.out.println("Browser" + ReadProperties.browserName() + "is not supported");
+                System.out.println("Browser " + ReadProperties.browserName() + " is not supported.");
                 break;
         }
     }
-    public WebDriver getDriver(){
+
+    public WebDriver getDriver() {
         driver.manage().window().maximize();
         driver.manage().deleteAllCookies();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+
         return this.driver;
     }
 
-    public ChromeOptions getChromeOptions(){
+    public ChromeOptions getChromeOptions() {
         ChromeOptions chromeOptions = new ChromeOptions();
+
         chromeOptions.setHeadless(false);
         chromeOptions.addArguments("--disable-gpu");
         chromeOptions.addArguments("--ignore-certificate-errors");
         chromeOptions.addArguments("--silent");
         chromeOptions.addArguments("--start-maximized");
         chromeOptions.addArguments("--incognito");
+
         return chromeOptions;
     }
-    public FirefoxOptions getFirefoxOptions(){
+
+    public FirefoxOptions getFirefoxOptions() {
         FirefoxOptions firefoxOptions = new FirefoxOptions();
+
         firefoxOptions.setHeadless(false);
         firefoxOptions.addArguments("--disable-gpu");
         firefoxOptions.addArguments("--ignore-certificate-errors");
         firefoxOptions.addArguments("--silent");
         firefoxOptions.addArguments("--start-maximized");
         firefoxOptions.addArguments("--incognito");
+
         return firefoxOptions;
     }
 }
