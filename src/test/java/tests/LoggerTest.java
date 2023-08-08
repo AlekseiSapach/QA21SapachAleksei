30d02c34c6531fd809d05c151beb9210b5020c8e
Aleksei1364 <sapach1364@gmail.com>
2023-08-08 16:56:04 +0300

    logger

--- src/test/java/tests/LoggerTest.java
+++ src/test/java/tests/LoggerTest.java
@@ -1,10 +1,11 @@
 package tests;
 
 import baseEntities.BaseTest;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.testng.annotations.Test;
-
+@Slf4j
 public class LoggerTest extends BaseTest {
     Logger logger = LogManager.getLogger(LoggerTest.class);
     @Test
@@ -15,5 +16,9 @@ public class LoggerTest extends BaseTest {
         logger.warn("Warning Message...");
         logger.error("Error Message...");
         logger.fatal("Fatal Message...");
+
+        log.info("Lombok - log message");
+        log.error("Lombok - log message");
+        log.warn("Lombok - log message");
     }
 }
