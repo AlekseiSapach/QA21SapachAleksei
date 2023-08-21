2f086a20ee94acf48c47d3ef0f5d6ddd1f4fad0f
Aleksei1364 <sapach1364@gmail.com>
2023-08-21 21:52:18 +0300

    Cucumber

--- src/test/java/stepDefs/FirstStepDefs.java
+++ src/test/java/stepDefs/FirstStepDefs.java
@@ -1,9 +1,51 @@
 package stepDefs;
 
-import io.cucumber.java.en.Given;
+import io.cucumber.java.en.*;
+import org.testng.Assert;
 
 public class FirstStepDefs {
     @Given("открыт браузер")
     public void startBrowser() {
+    }
+
+    @When("страница логина открыта")
+    public void openLoginPage() {
+    }
+
+    @Then("поле username отображается")
+    public void isUsernameFieldDisplayed() {
+        Assert.assertTrue(true);
+    }
+
+    @And("поле password отображается")
+    public void isPasswordFieldDisplayed() {
+        Assert.assertTrue(true);
+    }
+
+    @But("поле password не отображается")
+    public void isPasswordFieldNotDisplayed () {
+        Assert.assertTrue(true);
+    }
+
+    @When("user {string} when password {string} logged in")
+    public void userWhenPasswordLoggedIn(String username, String password) {
+        System.out.println("Username: "+ username);
+        System.out.println("Password: "+ password);
+    }
+
+    @When("^admin \"([^\"]*)\" when password \"([^\"]*)\" logged in$")
+    public void adminWhenPasswordLoggedIn(String username, String password) {
+        System.out.println("Username: "+ username);
+        System.out.println("Password: "+ password);
+    }
+
+    @Then("title is {string}")
+    public void titleIsAllProjects(String value) {
+        System.out.println("Title: "+ value);
+    }
+
+    @And("^project id is (\\d+)$")
+    public void projectIdIs(int value) {
+        System.out.println("Id: "+ value);
     }
 }
--- src/test/resources/features/first.feature
+++ src/test/resources/features/first.feature
@@ -4,3 +4,24 @@ Feature: Начальный тест для изучения Cucumber
 
   Scenario: Простой тест с Given
     Given открыт браузер
+
+  Scenario: Простой тест с Given и When
+    Given открыт браузер
+    When страница логина открыта
+
+  Scenario: Простой тест со всеми ключевыми словами
+    Given открыт браузер
+    When страница логина открыта
+    Then поле username отображается
+
+  Scenario: Использование END
+    Given открыт браузер
+    And страница логина открыта
+    Then поле username отображается
+    And поле password отображается
+
+  Scenario: Использование But
+    Given открыт браузер
+    And страница логина открыта
+    Then поле username отображается
+    But поле password не отображается
