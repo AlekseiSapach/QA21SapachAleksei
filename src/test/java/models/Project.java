ecda9d623da7771be1b2cca1fa3b371d94971ade
Aleksei1364 <sapach1364@gmail.com>
2023-08-30 22:05:09 +0300

    apiTest

---
+++ src/test/java/models/Project.java
@@ -0,0 +1,16 @@
+package models;
+
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+
+@Data
+public class Project {
+    @SerializedName(value = "name")
+    private String projectName;
+    private String Announcement;
+    @SerializedName(value = "suite_mode")
+    private int ProjectType;
+    @SerializedName(value = "show_announcement")
+    private boolean Flag;
+
+}
---
+++ src/test/java/tests/api/TestApiTest.java
@@ -0,0 +1,52 @@
+package tests.api;
+
+import baseEntities.BaseApiTest;
+import com.ctc.wstx.shaded.msv_core.datatype.xsd.ConcreteType;
+import io.restassured.http.ContentType;
+import io.restassured.http.Method;
+import io.restassured.response.Response;
+import io.restassured.specification.RequestSpecification;
+import org.apache.http.HttpStatus;
+import org.apache.http.protocol.HTTP;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import utils.configuration.ReadProperties;
+
+import static io.restassured.RestAssured.given;
+
+public class TestApiTest extends BaseApiTest {
+    static Logger logger = LogManager.getLogger(TestApiTest.class);
+
+    @Test
+    public void getAllUsers(){
+        String endpoint = "/index.php?/api/v2/get_users";
+        RequestSpecification httpRequest = given();
+        httpRequest.header(HTTP.CONTENT_TYPE, ContentType.JSON);
+        httpRequest.auth().preemptive().basic(ReadProperties.username(),ReadProperties.password());
+
+        logger.info(httpRequest.log().all());
+        Response response = httpRequest.request(Method.GET,endpoint);
+        Assert.assertEquals(response.getStatusCode(), HttpStatus.SC_OK);
+
+        logger.info(response.getBody().asPrettyString());
+        logger.info(httpRequest.log().all());
+
+    }
+
+    @Test
+    public void getAllUsers1(){
+        String endpoint = "/index.php?/api/v2/get_users";
+        given()
+                .auth().preemptive().basic(ReadProperties.username(),ReadProperties.password())
+                .header(HTTP.CONTENT_TYPE, ContentType.JSON)
+                .when()
+                .get(endpoint)
+                .then()
+                .log().status()
+                .log().body()
+                .statusCode(HttpStatus.SC_OK);
+
+    }
+}
