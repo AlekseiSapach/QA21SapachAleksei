ad5f37addc6c2762bda8fb485873f926729bdda0
Aleksei1364 <sapach1364@gmail.com>
2023-09-10 18:25:44 +0300

    apiTest HomeWork

--- src/test/java/utils/Endpoints.java
+++ src/test/java/utils/Endpoints.java
@@ -10,7 +10,7 @@ public class Endpoints {
     public static final String UPDATE_MILESTONE = "/index.php?/api/v2/update_milestone/{milestone_id}";
     public static final String DELETE_MILESTONE = "/index.php?/api/v2/delete_milestone/{milestone_id}";
     public static final String GET_CASE = "/index.php?/api/v2/get_case/{case_id}";
-    public static final String ADD_CASE = "/index.php?/api/v2/add_case/{section_id}";
+    public static final String ADD_CASE = "/index.php?/cases/add/{section_id}";
     public static final String UPDATE_CASE = "/index.php?/api/v2/update_case/{case_id}";
     public static final String DELETE_CASE = "/index.php?/api/v2/delete_case/{case_id}";
 
